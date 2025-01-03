77516166fec3102877a6b66ed125cd078f9c1e34
Rodrigo Tapia <rmtapia@gmail.com>
2025-01-02 21:45:14 -0300

    create folders and add menu buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import reactImg from './assets/react-core-concepts.png'
+
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1) );
-}
-
-function Header(){
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>
-  )
-}
-
-function CoreConcept({image, title, description}){
-  return(
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/header/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -44,6 +17,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>LALA</TabButton>
+            <TabButton>PO</TabButton>
+            <TabButton>TINKIWINKE</TabButton>
+            <TabButton>ROTO</TabButton>
+          </menu>
         </section>
       </main>
     </div>
